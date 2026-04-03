ad1a3cf2a23f82eb1f1b5c652269939791fe589f
iYc227 <110960135+iYc227@users.noreply.github.com>
2026-04-03 23:47:08 +0800

    更新 parking_modify.js

--- parking_modify.js
+++ parking_modify.js
@@ -1,9 +1,17 @@
-// 停车时间&金额修改脚本
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 改进场时间（格式：YYYY-MM-DD HH:mm:ss，可自行修改）
+// 总费用
+obj.data.data.amount = 4;
+
+// 进场时间
 obj.data.data.inTime = "2026-04-03 23:00:00";
+
+// 停车时长（秒）
+obj.data.data.useTime = 46;
+
+// 实付金额
+obj.data.data.payAmount = 4;
 
 body = JSON.stringify(obj);
 $done({ body });
