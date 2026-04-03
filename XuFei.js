df864afcf26c7219eb16840099887fb36e86e29a
iYc227 <110960135+iYc227@users.noreply.github.com>
2026-04-04 01:34:33 +0800

    更新 XuFei.js

--- XuFei.js
+++ XuFei.js
@@ -2,11 +2,11 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 核心：只改这2行，把所有套餐价格改成0元
-obj.data.packets.forEach(item => {
-  item.amount = 0;
-  item.discount_amount = 0;
-});
+// 只改第一个套餐（1个月）：时长改成12个月，
+obj.data.packets[0].amount = 0;
+obj.data.packets[0].value = 12;
+// 可选：把套餐名改成「12个月」，方便你识别（不改也不影响功能）
+obj.data.packets[0].packet_name = "一个月";
 
 // 固定结尾
 body = JSON.stringify(obj);
