0d1b5604eff166ed9f48c1fd8178251b0a500f00
iYc227 <110960135+iYc227@users.noreply.github.com>
2026-04-04 01:20:44 +0800

    更新 XuFei2.js

--- XuFei2.js
+++ XuFei2.js
@@ -6,7 +6,7 @@ let obj = JSON.parse(body);
 // 核心修改：只改车辆信息，套餐完全不动
 // ==============================================
 // 1. 把月卡到期时间改成永久（2099年12月31日，永远不过期）
-obj.data.vehicleinfo.end_time = "2027-4-30T23:59:59.000Z";
+obj.data.vehicleinfo.end_time = "2027-04-30T15:59:59.000Z";
 
 // 固定结尾
 body = JSON.stringify(obj);
