517e5738e81e1dca4c3db83cd614f8de115a5a0b
iYc227 <110960135+iYc227@users.noreply.github.com>
2026-04-04 01:39:30 +0800

    更新 XuFei.js

--- XuFei.js
+++ XuFei.js
@@ -3,7 +3,7 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 // 只改第一个套餐（1个月）：时长改成12个月，
-obj.data.packets[0].amount = 0;
+obj.data.packets[0].amount = 3000;
 obj.data.packets[0].value = 12;
 // 可选：把套餐名改成「12个月」，方便你识别（不改也不影响功能）
 obj.data.packets[0].packet_name = "一个月";
