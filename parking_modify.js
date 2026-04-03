be3d03daadec0abd5ef75a11dc8beac2b0d8d386
iYc227 <110960135+iYc227@users.noreply.github.com>
2026-04-03 22:08:00 +0800

    更新 parking_modify.js

--- parking_modify.js
+++ parking_modify.js
@@ -3,7 +3,7 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 // 改进场时间（格式：YYYY-MM-DD HH:mm:ss，可自行修改）
-obj.data.data.inTime = "2026-04-03 08:00:00";
+obj.data.data.inTime = "2026-04-03 22:00:00";
 
 // 改总金额为0（免费停车）
 obj.data.data.amount = 0;
