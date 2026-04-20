2cb80f53198a35563daf5a4671715b9ece99ee07
iYc227 <110960135+iYc227@users.noreply.github.com>
2026-04-20 15:40:59 +0800

    更新 XuFei.js

--- XuFei.js
+++ XuFei.js
@@ -1,15 +1,21 @@
 let body = $response.body;
 
-// 1. 修改余额为100
-body = body.replace(/"wash_balance":"0"/g, '"wash_balance":"100"');
+// 设备余额
+body = body.replace(/"wash_balance":0/g, '"wash_balance":100');
 
-// 2. 修改所有套餐价格为0.01元
+// 钱包余额（元）
+body = body.replace(/"balance":0/g, '"balance":100');
+
+// 钱包余额（分）
+body = body.replace(/"balance_fen":0/g, '"balance_fen":10000');
+
+// 套餐价格全部改成 0.01
 body = body.replace(/"rent_unit_fee":"28"/g, '"rent_unit_fee":"0.01"');
 body = body.replace(/"rent_unit_fee":"16"/g, '"rent_unit_fee":"0.01"');
 body = body.replace(/"rent_unit_fee":"10"/g, '"rent_unit_fee":"0.01"');
-body = body.replace(/"rent_unit_fee":"8"/g, '"rent_unit_fee":"0.01"');
+body = body.replace(/"rent_unit_fee":"8"/g, '"rent_unit_fee":"0"');
 
-// 3. 修改应付金额为1分
+// 应付金额改成 1 分
 body = body.replace(/"need_pay_fen":2800/g, '"need_pay_fen":1');
 
 $done({ body });
